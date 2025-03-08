d8844018cdbf79763c6530ca5691ad3533322ef9
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-08 12:37:09 +0530

    asteroid.rb file created

---
+++ asteroid.rb
@@ -0,0 +1,18 @@
+class Asteroid
+    WIDTH = 58
+    HEIGHT = 61
+    SPEEDS = (1..4).to_a
+    ROTATATIONS = (-2..2).to_a
+  
+    def initialize
+      scale = 0.5 + (rand * 1.5)
+      speed = SPEEDS.sample
+      @rotation = ROTATATIONS.sample
+      @sprite = Sprite.new(
+        'asteroid.png',
+        x: rand(Window.width),
+        y: rand(Window.height),
+        width: WIDTH * scale,
+        height: HEIGHT * scale,
+        rotate: rand(360)
+      )
