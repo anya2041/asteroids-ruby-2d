22913a6879afa5e5710a94045a5dcefaed34817a
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-08 12:45:18 +0530

    added move fn to asteroid.rb

--- asteroid.rb
+++ asteroid.rb
@@ -21,3 +21,22 @@ class Asteroid
       @y_velocity = -Math.cos(@sprite.rotate * Math::PI / 180) * speed
     end
   
+    def move
+        @sprite.rotate += @rotation
+        @sprite.x += @x_velocity
+        @sprite.y += @y_velocity
+    
+        if @sprite.x > Window.width + @sprite.width
+          @sprite.x = -@sprite.width
+        elsif @sprite.x < -@sprite.width
+          @sprite.x = Window.width + @sprite.width
+        end
+    
+        if @sprite.y > Window.height + @sprite.height
+          @sprite.y = -@sprite.height
+        elsif @sprite.y < -@sprite.height
+          @sprite.y = Window.height + @sprite.height
+        end
+      end
+    end
+    
