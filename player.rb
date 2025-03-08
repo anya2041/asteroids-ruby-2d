801d5ac95cf147c8423e1adcef30705d4f12770a
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-08 13:04:49 +0530

    modified player.rb

--- player.rb
+++ player.rb
@@ -7,3 +7,29 @@ class Player
     SLOW_DOWN_RATE = 0.99
   
     attr_reader :image, :x, :y, :speed, :fire_rate
+
+    def initialize(image, x, y, speed, fire_rate)
+        @x_velocity = 0
+        @y_velocity = 0
+        @image = image
+        @x = x
+        @y = y
+        @speed = speed
+        @fire_rate = fire_rate
+        @projectiles = []
+        @last_projectile_fired_frame = 0
+        @sprite = Sprite.new(
+          image,
+          clip_width: 32,
+          width: WIDTH,
+          height: HEIGHT,
+          x: x,
+          y: y,
+          rotate: 180,
+          animations: {
+            moving_slow: 1..2,
+            moving_fast: 3..4
+          }
+        )
+      end
+    
