45eae9ac3c6da8d6fa950bffd368bde13ae203fd
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-14 16:47:34 +0530

    added code

--- player_select_screen.rb
+++ player_select_screen.rb
@@ -13,3 +13,38 @@ class PlayerSelectScreen
         Player.new('player_2.png', Window.width * (2/4.0) - Player::WIDTH / 2, 240, 100, 60),
         Player.new('player_3.png', Window.width * (3/4.0) - Player::WIDTH / 2, 240, 60, 100),
       ]
+  
+      @selected_player = 1
+  
+      animate_players
+      add_player_masks
+      set_player_stat_text
+    end
+  
+    def update
+      if Window.frames % 2 == 0
+        @stars.each { |star| star.move }
+      end
+    end
+
+    def animate_players
+        @players.each_with_index do |player, index|
+          if index == @selected_player
+            player.animate_fast
+          else
+            player.animate_slow
+          end
+        end
+      end
+    
+      def move(direction)
+        if direction == :left
+          @selected_player = (@selected_player - 1) % 3
+        else
+          @selected_player = (@selected_player + 1) % 3
+        end
+    
+        animate_players
+        add_player_masks
+        set_player_stat_text
+      end
