dad0d0b045e47c85598d71073f24592a532aad1c
Anya 2022088 <127613201+anya2041@users.noreply.github.com>
2025-03-14 16:48:48 +0530

    added projectile.rb

--- player_select_screen.rb
+++ player_select_screen.rb
@@ -48,3 +48,54 @@ class PlayerSelectScreen
         add_player_masks
         set_player_stat_text
       end
+
+      def add_player_masks
+        @player_masks && @player_masks.each { |mask| mask.remove }
+    
+        @player_masks = @players.each_with_index.map do |player, index|
+          if index == @selected_player
+            color = [0.2, 0.2, 0.2, 0.6]
+            z = -1
+          else
+            color = [0.0, 0.0, 0.0, 0.6]
+            z = 2
+          end
+    
+          Circle.new(
+            radius: 100,
+            sectors: 32,
+            x: player.x + (Player::WIDTH / 2),
+            y: player.y + (Player::HEIGHT / 2),
+            color: color,
+            z: z
+          )
+        end
+      end
+
+      def set_player_stat_text
+        @player_stat_texts && @player_stat_texts.each { |text| text.remove }
+    
+        @player_stat_texts = []
+        @players.each_with_index do |player, index|
+          if index == @selected_player
+            color = Color.new([1,1,1,1])
+          else
+            color = Color.new([0.3,0.3,0.3,1])
+          end
+    
+          speed_text = Text.new("Speed - #{player.speed}%", size: 20, y: player.y + 200, color: color)
+          speed_text.x = player.x + ((Player::WIDTH - speed_text.width)/2)
+    
+          fire_rate_text = Text.new("Fire rate - #{player.fire_rate}%", size: 20, y: player.y + 220, color: color)
+          fire_rate_text.x = player.x + ((Player::WIDTH - fire_rate_text.width)/2)
+    
+          @player_stat_texts.push(speed_text)
+          @player_stat_texts.push(fire_rate_text)
+        end
+    
+      end
+    
+      def selected_player
+        @players[@selected_player]
+      end
+    end
